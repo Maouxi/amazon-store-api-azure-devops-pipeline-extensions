05d9ac91e8e0e13fc812483035d1684ab9df13b3
Maxence Raoux <maxence.raoux@live.fr>
2020-06-05 10:27:42 +0200

    Improve logs

--- amazonStoreAuth/amazonStoreAuthTask/index.js
+++ amazonStoreAuth/amazonStoreAuthTask/index.js
@@ -13,7 +13,7 @@ const tl = require("azure-pipelines-task-lib/task");
 const querystring = require("querystring");
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        console.log('Start authentication');
+        console.log('- Start authentication');
         try {
             var request = require('sync-request');
             const clientId = tl.getInput('clientId', true);
@@ -37,18 +37,19 @@ function run() {
                 'body': data
             };
             var res = request('POST', `https://api.amazon.com/auth/o2/token`, options);
-            console.log(`Api call status code: ${res.statusCode}`);
+            console.log(`POST - Authenticate .Code: ${res.statusCode}`);
             if (res.statusCode = 200) {
                 var obj = JSON.parse(res.getBody().toString());
+                console.log(`POST - Authenticate .Code: ${res.statusCode}`);
                 tl.setVariable('AmazonAuthToken', `${obj.access_token}`);
                 tl.setResult(tl.TaskResult.Succeeded, "Success");
             }
             else {
-                throw `Fail to authenticate :${res.statusCode}`;
+                throw `POST - Authenticate fail. Code: ${res.statusCode}`;
             }
         }
         catch (err) {
-            console.log(`error: ${err}`);
+            console.log(`- Error: ${err}`);
             tl.setResult(tl.TaskResult.Failed, err.message);
         }
     });
--- amazonStoreAuth/amazonStoreAuthTask/index.ts
+++ amazonStoreAuth/amazonStoreAuthTask/index.ts
@@ -3,7 +3,8 @@ import https = require('https')
 import querystring = require('querystring');
 
 async function run() {
-    console.log('Start authentication');
+    console.log('- Start authentication');
+
     try {
         var request = require('sync-request');
 
@@ -30,18 +31,20 @@ async function run() {
         };
 
         var res = request('POST', `https://api.amazon.com/auth/o2/token`, options);
-        console.log(`Api call status code: ${res.statusCode}`);
+        console.log(`POST - Authenticate .Code: ${res.statusCode}`);
 
         if (res.statusCode = 200) {
             var obj = JSON.parse(res.getBody().toString());
+            console.log(`POST - Authenticate .Code: ${res.statusCode}`);
+
             tl.setVariable('AmazonAuthToken', `${obj.access_token}`);
             tl.setResult(tl.TaskResult.Succeeded, "Success");
         } else {
-            throw `Fail to authenticate :${res.statusCode}`;
+            throw `POST - Authenticate fail. Code: ${res.statusCode}`;
         }
     }
     catch (err) {
-        console.log(`error: ${err}`);
+        console.log(`- Error: ${err}`);
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
