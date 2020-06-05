02f0e20905784b4b0c2d7b51e491f421dd837ff9
Maxence Raoux <maxence.raoux@live.fr>
2020-06-05 10:27:11 +0200

    Begin apk upload. Not working yet

--- amazonStoreEdit/amazonStoreEditTask/index.js
+++ amazonStoreEdit/amazonStoreEditTask/index.js
@@ -10,11 +10,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const tl = require("azure-pipelines-task-lib/task");
+const fs = require("fs");
 var endpoint = "https://developer.amazon.com/api/appstore/v1/applications";
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         console.log('Start create or edit');
-        var token = "Atc|MQEBIMpQ50I_d9jeJ_viBKfq2yBE6IwnMDqgcGHCYbuKtWRYpAHtcWeDSHmD48yQtSbuU-O8f3mcgFutDyTzgkXxYBB5rw02tzTraB1oCi8iTsYfASNoY69Pl8PA-7VrI4Z79GeJDrH51JAL_2jIP1xCVaNEuEOSUzCx3HdAWpytafCpSzy8uk5v0X2iuTzJZZN8hLF25wKpbulndPBYqUVDxRit-hi0EfAEb7h0LyiXit9XSIKsJiDOvT2fBUG2Tlh2Ifk9GNqTLczYzIICOa42C1qZqEsS3ZK585rMPa7pcztLUA";
+        //TODO: remove this line
+        var token = "Atc|MQEBIB6jGF7b4nLJImMsnWWZL_y10C5KbGVe_GVYFecBjA_ihM3S9pe_ITE6lTF10wSM6HuoqxeaKDNIm1cV_k_ku-gmQ8L1tX1uXp4OtqxdnNbVxcxu29UqFPHLsdHh6g0f64GUExBl0zDXQM31rmuVTIAo20irc8HqRDv0XtbsHzRnShh77jT_JGv2dwvRwpVcjrwp9kLUF9Glcmc3iGla7fz3J6TqZW6T5-YRf7oTuaB2AVC7Mvu7_qFXRvkfnEBZFvmRN4gUO6wZfYeL_WbbD479kBrgA-f0rja4yA2xe5u8eg";
         //var token = tl.getVariable("AmazonAuthTask.AmazonAuthToken");
         if (token == undefined) {
             tl.setResult(tl.TaskResult.Failed, `You need to use the Auth task first to get a valid access_token`);
@@ -25,18 +27,23 @@ function run() {
             tl.setResult(tl.TaskResult.Failed, 'AppId is required');
             return;
         }
-        const apkFilePath = tl.getInput('apkFilePath', true);
+        const apkFilePath = tl.getPathInput('apkFilePath', true);
         if (apkFilePath == undefined) {
             tl.setResult(tl.TaskResult.Failed, 'Apk file path is required');
             return;
         }
         try {
+            //Get or create active edit
+            console.log(`- Start create or edit for appId: ${appId}`);
             var editId = getActiveEdit(appId, token);
-            var result = updateApk(appId, editId, apkFilePath, token);
+            //Update the current edit with a new apk
+            console.log(`- Start upload apk: ${apkFilePath}`);
+            updateApk(appId, editId, apkFilePath, token);
+            //End update
             tl.setResult(tl.TaskResult.Succeeded, `Successfully update app ${editId}`);
         }
         catch (err) {
-            console.log(`error: ${err}`);
+            console.log(`- Error: ${err}`);
             tl.setResult(tl.TaskResult.Failed, err.message);
         }
     });
@@ -45,48 +52,54 @@ function getActiveEdit(appId, token) {
     var request = require('sync-request');
     var options = { 'headers': { 'Authorization': `bearer ${token}`, "accept": "application/json" } };
     var res = request('GET', `${endpoint}/${appId}/edits`, options);
-    console.log(`Active edits status code: ${res.statusCode}`);
+    console.log(`GET - Retrieve active edits. Code: ${res.statusCode}`);
     if (res.statusCode == 200) {
         var obj = JSON.parse(res.getBody().toString());
-        console.log(`Find an update ${obj.status} id = ${obj.id}`);
+        console.log(`GET - Retrieve active edits success. Status: ${obj.status} | Id: ${obj.id}`);
         return obj.id;
     }
     else if (res.statusCode == 404) {
-        console.log(`No update exist. Create a new one`);
+        console.log(`GET - Retrieve active edits not found.`);
         return createNewEdit(appId, token);
     }
     else {
-        throw `Fail to check active update: ${res.statusCode}`;
+        throw `GET - Retrieve active edits fail. Code: ${res.statusCode}`;
     }
 }
 function createNewEdit(appId, token) {
     var request = require('sync-request');
     var options = { 'headers': { 'Authorization': `bearer ${token}`, "accept": "application/json" } };
     var res = request('POST', `${endpoint}/${appId}/edits`, options);
-    console.log(`Active edits status code: ${res.statusCode}`);
+    console.log(`POST - Create new edits. Code: ${res.statusCode}`);
     if (res.statusCode == 200) {
         var obj = JSON.parse(res.getBody().toString());
-        console.log(`Create an update ${obj.status} id = ${obj.id}`);
+        console.log(`POST - Create new edits success. Status: ${obj.status} | Id: ${obj.id}`);
         return obj.id;
     }
     else {
-        throw `Fail to create update: ${res.statusCode}`;
+        throw `POST - Create new edits. Code: ${res.statusCode}`;
     }
 }
 function updateApk(appId, editId, apkFilePath, token) {
+    var _a, _b, _c;
     var request = require('sync-request');
-    console.log(`Upload apk: ${apkFilePath}`);
-    var data = apkFilePath; //TODO
+    var fileBuffer = fs.readFileSync(apkFilePath, { encoding: 'utf8', flag: 'r' });
     var options = {
         'headers': {
             'Authorization': `bearer ${token}`,
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': data.length
+            'Content-Length': fileBuffer.length,
+            'fileName': (_c = (_b = (_a = apkFilePath.split('\\')) === null || _a === void 0 ? void 0 : _a.pop()) === null || _b === void 0 ? void 0 : _b.split('/').pop()) === null || _c === void 0 ? void 0 : _c.toString()
         },
-        'body': data
+        'body': fileBuffer
     };
     var res = request('POST', `${endpoint}/${appId}/edits/${editId}/apks/upload`, options);
-    throw "Not implemented yet"; //TODO
-    return false;
+    console.log(`POST - Upload apk. Code: ${res.statusCode}`);
+    if (res.statusCode == 200) {
+        console.log(`POST - Upload apk success`);
+    }
+    else {
+        throw `POST - Upload apk fail. Code: ${res.statusCode}`;
+    }
 }
 run();
--- amazonStoreEdit/amazonStoreEditTask/index.ts
+++ amazonStoreEdit/amazonStoreEditTask/index.ts
@@ -1,12 +1,14 @@
 import tl = require('azure-pipelines-task-lib/task');
 import https = require('https')
 import querystring = require('querystring');
+import fs = require('fs');
 
 var endpoint = "https://developer.amazon.com/api/appstore/v1/applications";
 
 async function run() {
     console.log('Start create or edit');
-    var token = "Atc|MQEBIMpQ50I_d9jeJ_viBKfq2yBE6IwnMDqgcGHCYbuKtWRYpAHtcWeDSHmD48yQtSbuU-O8f3mcgFutDyTzgkXxYBB5rw02tzTraB1oCi8iTsYfASNoY69Pl8PA-7VrI4Z79GeJDrH51JAL_2jIP1xCVaNEuEOSUzCx3HdAWpytafCpSzy8uk5v0X2iuTzJZZN8hLF25wKpbulndPBYqUVDxRit-hi0EfAEb7h0LyiXit9XSIKsJiDOvT2fBUG2Tlh2Ifk9GNqTLczYzIICOa42C1qZqEsS3ZK585rMPa7pcztLUA";
+    //TODO: remove this line
+    var token = "Atc|MQEBIB6jGF7b4nLJImMsnWWZL_y10C5KbGVe_GVYFecBjA_ihM3S9pe_ITE6lTF10wSM6HuoqxeaKDNIm1cV_k_ku-gmQ8L1tX1uXp4OtqxdnNbVxcxu29UqFPHLsdHh6g0f64GUExBl0zDXQM31rmuVTIAo20irc8HqRDv0XtbsHzRnShh77jT_JGv2dwvRwpVcjrwp9kLUF9Glcmc3iGla7fz3J6TqZW6T5-YRf7oTuaB2AVC7Mvu7_qFXRvkfnEBZFvmRN4gUO6wZfYeL_WbbD479kBrgA-f0rja4yA2xe5u8eg";
     //var token = tl.getVariable("AmazonAuthTask.AmazonAuthToken");
     if (token == undefined) {
         tl.setResult(tl.TaskResult.Failed, `You need to use the Auth task first to get a valid access_token`);
@@ -17,74 +19,80 @@ async function run() {
         tl.setResult(tl.TaskResult.Failed, 'AppId is required');
         return;
     }
-    const apkFilePath: string | undefined = tl.getInput('apkFilePath', true);
+    const apkFilePath: string | undefined = tl.getPathInput('apkFilePath', true);
     if (apkFilePath == undefined) {
         tl.setResult(tl.TaskResult.Failed, 'Apk file path is required');
         return;
     }
 
     try {
+        //Get or create active edit
+        console.log(`- Start create or edit for appId: ${appId}`);
         var editId = getActiveEdit(appId, token);
-
-        var result = updateApk(appId, editId, apkFilePath, token);
-
+        //Update the current edit with a new apk
+        console.log(`- Start upload apk: ${apkFilePath}`);
+        updateApk(appId, editId, apkFilePath, token);
+        //End update
         tl.setResult(tl.TaskResult.Succeeded, `Successfully update app ${editId}`);
     }
     catch (err) {
-        console.log(`error: ${err}`);
+        console.log(`- Error: ${err}`);
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
 
-function getActiveEdit(appId: String, token: String): String {
+function getActiveEdit(appId: string, token: string): string {
     var request = require('sync-request');
     var options = { 'headers': { 'Authorization': `bearer ${token}`, "accept": "application/json" } };
     var res = request('GET', `${endpoint}/${appId}/edits`, options);
 
-    console.log(`Active edits status code: ${res.statusCode}`);
+    console.log(`GET - Retrieve active edits. Code: ${res.statusCode}`);
     if (res.statusCode == 200) {
         var obj = JSON.parse(res.getBody().toString());
-        console.log(`Find an update ${obj.status} id = ${obj.id}`);
+        console.log(`GET - Retrieve active edits success. Status: ${obj.status} | Id: ${obj.id}`);
         return obj.id;
     } else if (res.statusCode == 404) {
-        console.log(`No update exist. Create a new one`);
+        console.log(`GET - Retrieve active edits not found.`);
         return createNewEdit(appId, token);
     } else {
-        throw `Fail to check active update: ${res.statusCode}`;
+        throw `GET - Retrieve active edits fail. Code: ${res.statusCode}`;
     }
 }
 
-function createNewEdit(appId: String, token: String): String {
+function createNewEdit(appId: string, token: string): string {
     var request = require('sync-request');
     var options = { 'headers': { 'Authorization': `bearer ${token}`, "accept": "application/json" } };
     var res = request('POST', `${endpoint}/${appId}/edits`, options);
 
-    console.log(`Active edits status code: ${res.statusCode}`);
+    console.log(`POST - Create new edits. Code: ${res.statusCode}`);
     if (res.statusCode == 200) {
         var obj = JSON.parse(res.getBody().toString());
-        console.log(`Create an update ${obj.status} id = ${obj.id}`);
+        console.log(`POST - Create new edits success. Status: ${obj.status} | Id: ${obj.id}`);
         return obj.id;
     } else {
-        throw `Fail to create update: ${res.statusCode}`;
+        throw `POST - Create new edits. Code: ${res.statusCode}`;
     }
 }
 
-function updateApk(appId: String, editId: String, apkFilePath: String, token: String): Boolean {
+function updateApk(appId: string, editId: string, apkFilePath: string, token: string) {
     var request = require('sync-request');
-    console.log(`Upload apk: ${apkFilePath}`);
-
-    var data = apkFilePath; //TODO
+    var fileBuffer = fs.readFileSync(apkFilePath, { encoding: 'utf8', flag: 'r' })
     var options = {
         'headers': {
             'Authorization': `bearer ${token}`,
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': data.length
+            'Content-Length': fileBuffer.length,
+            'fileName': apkFilePath.split('\\')?.pop()?.split('/').pop()?.toString()
         },
-        'body': data
+        'body': fileBuffer
     };
     var res = request('POST', `${endpoint}/${appId}/edits/${editId}/apks/upload`, options);
-    throw "Not implemented yet" //TODO
-    return false
+    console.log(`POST - Upload apk. Code: ${res.statusCode}`);
+    if (res.statusCode == 200) {
+        console.log(`POST - Upload apk success`);
+    } else {
+        throw `POST - Upload apk fail. Code: ${res.statusCode}`
+    }
 }
 
 run();
